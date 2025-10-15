bc5db3a4b7fed4192b20151f23eccef62676f6a5
Itamar Junior <hello@itjunior.dev>
2025-10-15 21:16:26 +0100

    Remove call-orchestrator2.0 application files and configurations

--- apps/call-orchestrator2.0/src/config/env.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const env = {
-  WEB_API_BASE_URL: process.env.WEB_API_BASE_URL || "",
-  API_TOKEN: process.env.API_TOKEN || "",
-  GROUP_ID: Number(process.env.GROUP_ID || 0) || undefined,
-};
--- apps/call-orchestrator2.0/src/config/logger.ts
+++
@@ -1,5 +0,0 @@
-export const logger = {
-  info: (...args: any[]) => console.log(...args),
-  error: (...args: any[]) => console.error(...args),
-  warn:  (...args: any[]) => console.warn(...args),
-};
